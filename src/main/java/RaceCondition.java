7eab7d08fcabb99c231f06e6a74bad4696cc2d76
mpakhomov <mike.pakhomov@gmail.com>
2015-06-16 17:42:04 +0300

    added RaceCondition example

---
+++ src/main/java/RaceCondition.java
@@ -0,0 +1,28 @@
+/**
+ * @author mpakhomov
+ * @since: 6/16/2015
+ */
+public class RaceCondition {
+    // race condition
+    private static boolean done;
+
+    // fix
+//   private volatile static boolean done;
+
+
+    public static void main(final String[] args) throws InterruptedException{
+        new Thread(
+                new Runnable() {
+                    public void run() {
+                        int i = 0;
+                        while(!done) { i++; }
+                        System.out.println("Done!");
+                    }
+                }
+        ).start();
+        System.out.println("OS: " + System.getProperty("os.name"));
+        Thread.sleep(2000);
+        done = true;
+        System.out.println("flag done set to true");
+    }
+}
