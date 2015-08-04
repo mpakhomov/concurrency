bec44f0ad91e1d1db192d7077783d4f3f7e50a0d
mpakhomov <mike.pakhomov@gmail.com>
2015-08-04 17:54:26 +0300

    Implemented incrementAndGet using sun.misc.unsafe

--- src/main/java/com/mpakhomov/Cas.java
+++ src/main/java/com/mpakhomov/Cas.java
@@ -1,18 +1,72 @@
 package com.mpakhomov;
 
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
+ * This class illustrates how AtomicInteger is implemented.
+ *
  * @author mpakhomov
  * @since 7/8/14
  */
 public class Cas {
-    AtomicInteger counter = new AtomicInteger(0);
+    // setup to use Unsafe.compareAndSwapInt for updates
+    private static final Unsafe unsafe = getUnsafe();
+    private static final long valueOffset;
+
+    static {
+        try {
+            valueOffset = unsafe.objectFieldOffset
+                    (AtomicInteger.class.getDeclaredField("value"));
+        } catch (Exception ex) { throw new Error(ex); }
+    }
+
+    private volatile int value = 0;
+
+    /**
+     * Atomically update Java variable to <tt>x</tt> if it is currently
+     * holding <tt>expected</tt>.
+     * @return <tt>true</tt> if successful
+     */
+//    public final native boolean compareAndSwapInt(Object o, long offset,
+//                                                  int expected,
+//                                                  int x);
+
+    /**
+     * Atomically increments by one the current value.
+     *
+     * @return the updated value
+     */
+    public final int incrementAndGet() {
+        int v;
+        do {
+//            v = unsafe.getIntVolatile(this, valueOffset);
+            v = value;
+        } while (!unsafe.compareAndSwapInt(this, valueOffset, v, v + 1));
+        return v;
+    }
+
+    @SuppressWarnings("restriction")
+    private static Unsafe getUnsafe() {
+        try {
+
+            Field singleoneInstanceField = Unsafe.class.getDeclaredField("theUnsafe");
+            singleoneInstanceField.setAccessible(true);
+            return (Unsafe) singleoneInstanceField.get(null);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 
     public static void main(String[] args) {
         Cas cas = new Cas();
-        AtomicInteger counter =  cas.counter;
-        counter.compareAndSet(1, 2);
-        System.out.println(counter);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(cas.incrementAndGet());
+        }
     }
 }
